e5e3fa0b860c68b90cd7abd21c98046efb45dad2
NatnaelH <natnaelhabtamu0@gmail.com>
2022-07-11 13:22:29 +0300

    Finishing up

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import useWindowDimensions from './components/useWindowDimensions';
-import {Table , Group, Button, Stack, Modal, Loader ,ScrollArea } from '@mantine/core'
-import { DoubleArrowRightIcon } from '@radix-ui/react-icons';
+import {Table , Group, Button, Stack, Modal, Loader ,ScrollArea, Text } from '@mantine/core'
+import { DoubleArrowRightIcon, ReaderIcon } from '@radix-ui/react-icons';
+import data from "./mockData";
 
-
-const elements = [
-  { id:1,  salary: "$150,000",   documents:['Offer',  'Employment', 'Assignment'] },
-  { id:2,  salary: "$150,000",   documents:['Offer','Employment','Assignment',"Email Addresses",'Phone Numbers',"Social Media Accounts","People Informations", "Health Status","People Informations", "Health Status"] },
-  { id:3,  salary: "$150,000",   documents:['Offer', 'Employment',  'Assignment',"Email Addresses", 'Random Files',] },
-  { id:11,  salary: "$150,000",   documents:['Offer',  'Employment', 'Assignment'] },
-  { id:12,  salary: "$150,000",   documents:['Offer','Employment','Assignment',"Email Addresses",'Phone Numbers',"Social Media Accounts","People Informations", "Health Status","People Informations", "Health Status"] },
-  { id:13,  salary: "$150,000",   documents:['Offer', 'Employment',  'Assignment',"Email Addresses", 'Random Files',] },
-
-];
 
 function App() {
   const [opened, setOpened] = useState(false);
@@ -28,34 +18,38 @@ function App() {
   }
   // console.log({height, width})
 
-  const rows = elements.map((element,index) => (
-    <tr key={element.id}>
-      <td  align='left'>{element.salary}</td>
-      <td align='left'>{(width <= 600 && element.documents.length >= 3 )|| 
-        (width <= 800 && element.documents.length >= 5 ) ||
-        (width <= 1000 && element.documents.length >= 7 ) ||
-        (width <= 1200 && element.documents.length >= 8 ) ||
-        (width <= 1400 && element.documents.length >= 10) ?
+  const rows = data.map((items,index) => (
+    <tr key={items.id}>
+      <td  align='left'><Text>{items.salary}</Text></td>
+      <td align='left'>{
+        (width <= 600 && items.documents.length >= 3 )|| 
+        (width <= 800 && items.documents.length >= 5 ) ||
+        (width <= 1000 && items.documents.length >= 7 ) ||
+        (width <= 1200 && items.documents.length >= 8 ) ||
+        (width <= 1400 && items.documents.length >= 10) ?
         <div>
-        <Button color="indigo" 
-          onClick={()=>handleDisplayVertically(element.documents)}>Documents <DoubleArrowRightIcon style={{marginLeft:'5'}}/></Button>
+        <Button color="dark" 
+          onClick={()=>handleDisplayVertically(items.documents)}>Documents <DoubleArrowRightIcon style={{marginLeft:'5'}}/></Button>
         </div>
        :
         // const identifiers = Object.keys(docs).slice(1)
         <Group > 
-          {element.documents.map((docs,index)=>{
-          return (<div key={index}  style={{color:'blue', display:'inline-flex', fontWeight:'bold'}}><p>{element.documents[index]}</p></div>)}
+          {items.documents.map((docs,index)=>{
+          return (<div key={index}  
+            style={{backgroundColor:'#f8f9fa', cursor:'pointer', borderRadius:5, color:'blue', display:'inline-flex', alignItems:'center', fontWeight:'bold'}}>
+              <ReaderIcon color='black'/><Text>{items.documents[index]}</Text></div>)}
           )}
         </Group>
+        
         } 
       </td>
     </tr>
   ));
 
   const ths = (
-    <tr>
-      <th>Salary</th>
-      <th>Documents</th>
+    <tr style={{fontWeight:'bold'}}>
+      <th><Text>Salary</Text> </th>
+      <th><Text>Documents</Text></th>
     </tr>
     );
   return (
@@ -68,11 +62,11 @@ function App() {
       </Table>
 
       <Modal opened={opened} onClose={()=>setOpened(false)}>
-        <ScrollArea sx={{height: 300}}>
+        <ScrollArea >
         <Stack  align="center" 
-         sx={(theme) => ({ backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] })}>
+         sx={(theme) => ({ fontWeight:'bold' , height: 150})}>
           {verticalDocs.length > 0 ? verticalDocs.map((docs,index)=>{
-            return <p key={index}>{docs}</p>
+            return (<Text key={index} sx={(theme)=>({ cursor:'pointer', color:'blue',backgroundColor: theme.colors.gray[0]})}><ReaderIcon color='black'/> {docs}</Text>)
           }): <Loader />}
         </Stack>
         </ScrollArea>
---
+++ src/mockData.js
@@ -0,0 +1,36 @@
+const data = [
+  {
+    id: 1,
+    salary: "$150,000",
+    documents: ["Offer.pdf", "Employment.pdf", "Assignment.pdf"],
+  },
+  {
+    id: 2,
+    salary: "$150,000",
+    documents: [
+      "Offer.pdf",
+      "Employment.pdf",
+      "Assignment.pdf",
+      "Email Addresses.pdf",
+      "Phone Numbers.pdf",
+      "Social Media Accounts.pdf",
+      "People Informations.pdf",
+      "Health Status.pdf",
+      "People Informations.pdf",
+      "Health Status.pdf",
+    ],
+  },
+  {
+    id: 3,
+    salary: "$150,000",
+    documents: [
+      "Offer.pdf",
+      "Employment.pdf",
+      "Assignment.pdf",
+      "Email Addresses.pdf",
+      "Random Files.pdf",
+    ],
+  },
+];
+
+export default data;
